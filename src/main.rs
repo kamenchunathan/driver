a95744bb5eba1626eb87ef80bf02d51b55eebaf0
Nathan Kamenchu <kamenchunathan@gmail.com>
2023-10-05 17:25:07 +0300

    feat: Add tracing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use bevy::{prelude::*, window::close_on_esc};
+use tracing_subscriber::{prelude::*, EnvFilter};
 
 use driver::DriverGamePlugins;
 
 fn main() {
+    init_tracing();
+
     App::new()
         .add_plugins((
             DefaultPlugins
@@ -18,4 +21,28 @@ fn main() {
         ))
         .add_systems(Update, close_on_esc)
         .run()
+}
+
+fn init_tracing() {
+    let now = time::OffsetDateTime::now_utc();
+
+    let (file_appender, _) = tracing_appender::non_blocking(tracing_appender::rolling::minutely(
+        "log",
+        format!("{now}"),
+    ));
+
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::fmt::layer()
+                .json()
+                .with_writer(file_appender)
+                .with_filter(EnvFilter::new(
+                    "debug,wgpu_core=warn,wgpu_hal=warn,naga=info,naga_oil=info",
+                )),
+        )
+        .with(
+            tracing_subscriber::fmt::layer()
+                .with_filter(EnvFilter::new("info,wgpu_core=warn,wgpu_hal=warn")),
+        )
+        .init();
 }
