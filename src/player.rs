be0ea971f032670368b26ea1cadf78ead3d8ae48
Nathan Kamenchu <kamenchunathan@gmail.com>
2023-10-08 11:14:06 +0300

    fix: Split the spritesheet into a texture atlas

--- src/player.rs
+++ src/player.rs
@@ -5,17 +5,32 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup);
+        app.add_systems(Startup, (spawn_player));
     }
 }
 
-fn setup(mut commands: Commands, asset_server: ResMut<AssetServer>) {
-    let player_texture_atlas = asset_server.load("Actor/Animals/Cat/SpriteSheet.png");
+fn spawn_player(
+    mut commands: Commands,
+    asset_server: ResMut<AssetServer>,
+    mut assets: ResMut<Assets<TextureAtlas>>,
+) {
+    let sprite_sheet = asset_server.load("Actor/Characters/BlueNinja/SpriteSheet.png");
+
+    let texture_atlas = assets.add(TextureAtlas::from_grid(
+        sprite_sheet,
+        Vec2::new(16.0, 16.0),
+        4,
+        7,
+        None,
+        None,
+    ));
+
     commands.spawn((
         SpriteSheetBundle {
-            texture_atlas: player_texture_atlas,
-            transform: Transform {
-                translation: Vec3::new(0.0, 10.0, 0.0),
+            texture_atlas,
+            sprite: TextureAtlasSprite {
+                index: 0,
+                custom_size: Some(Vec2::new(32.0, 32.0)),
                 ..default()
             },
             ..default()
