79aeee2c519ac78d0175ebef749da74cdad9bcab
Nathan Kamenchu <kamenchunathan@gmail.com>
2023-10-09 09:33:41 +0300

    feat: Add animation to the sprite

--- src/player.rs
+++ src/player.rs
@@ -1,11 +1,30 @@
+use std::time::Duration;
+
 use bevy::log::info;
 use bevy::prelude::*;
+use bevy::time::Timer;
 
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, (spawn_player));
+        app.add_systems(Startup, (spawn_player))
+            .add_systems(Update, animate_player);
+    }
+}
+
+#[derive(Debug, Component, Reflect)]
+struct AnimationPlayer {
+    frame_time: f32,
+    timer: Timer,
+}
+
+impl AnimationPlayer {
+    fn new(frame_time: f32) -> Self {
+        Self {
+            frame_time,
+            timer: Timer::new(Duration::from_secs_f32(frame_time), TimerMode::Repeating),
+        }
     }
 }
 
@@ -26,15 +45,28 @@ fn spawn_player(
     ));
 
     commands.spawn((
+        Name::new("Player"),
         SpriteSheetBundle {
             texture_atlas,
             sprite: TextureAtlasSprite {
-                index: 0,
+                index: 1,
                 custom_size: Some(Vec2::new(32.0, 32.0)),
                 ..default()
             },
             ..default()
         },
-        Name::new("Player"),
+        AnimationPlayer::new(0.4),
     ));
+}
+
+fn animate_player(mut player: Query<(&mut TextureAtlasSprite, &mut AnimationPlayer)>, time: Res<Time>) {
+    if let Ok((mut sprite, mut player)) = player.get_single_mut() {
+        player
+            .timer
+            .tick(Duration::from_secs_f32(time.delta_seconds()));
+
+        if player.timer.just_finished() {
+            sprite.index = (sprite.index + 4) % 13;
+        }
+    }
 }
